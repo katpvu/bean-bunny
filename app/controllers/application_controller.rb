b9a99df7ac82df1f52575e87c0a6caca2047b3d1
katpvu <vu.kathy98@gmail.com>
2023-04-21 11:38:37 -0700

    add application controller helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 class ApplicationController < ActionController::API
     before_action :snake_case_params
 
+    # CRRLLL
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token]) 
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!(user)
+        current_user.reset_session_token! if current_user
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: { message: 'Unauthorized' }, status: :unauthorized
+        end
+    end
+
     private
 
     def snake_case_params

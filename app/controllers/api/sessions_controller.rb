6f3b492db1f9e3e5788f22d813f538fe03786102
katpvu <vu.kathy98@gmail.com>
2023-04-21 15:03:59 -0700

    add csrf protection and error handling for it

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Api::SessionsController < ApplicationController
     # retrieving current user GET '/api/session'
     def show
+        banana
         if current_user
             @user = current_user
             render 'api/users/show'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::API
-    before_action :snake_case_params
+    include ActionController::RequestForgeryProtection
+  
+    protect_from_forgery with: :exception
+    before_action :snake_case_params, :attach_authenticity_token
     helper_method :current_user
+
+    rescue_from StandardError, with: :unhandled_error
+    rescue_from ActionController::InvalidAuthenticityToken, 
+        with: :invalid_authenticity_token
 
     def test
         if params.has_key?(:login)
@@ -41,5 +48,31 @@ class ApplicationController < ActionController::API
 
     def snake_case_params
     params.deep_transform_keys!(&:underscore)
+    end
+
+    def attach_authenticity_token
+        headers['X-CSRF-Token'] = masked_authenticity_token(session)
+    end
+
+    # whenever an invalid authenticity token error is raised, this method will run and render a JSON response
+    def invalid_authenticity_token
+        render json: { message: 'Invalid authenticity token' }, 
+            status: :unprocessable_entity
+    end
+      
+    # any other error raised: this is run 
+    def unhandled_error(error)
+        # check if req is looking for HTML resp based on its Accepts header; if so, re-raise error
+        if request.accepts.first.html?
+            raise error
+        else # render JSON response containing info about the error using the internal_server_error jbuilder
+            # create custom @message and @stack instance vars to use in views
+            @message = "#{error.class} - #{error.message}"
+            @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+            render 'api/errors/internal_server_error', status: :internal_server_error
+            
+            #after rendering, use logger.error to log the error message and stack trace in server log
+            logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+        end
     end
 end

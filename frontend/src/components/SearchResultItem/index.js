04046ecbba4919551d478c0efbd63fa6476f6644
kat vu <vu.kathy98@gmail.com>
2023-04-25 13:35:14 -0700

    add business page details

--- frontend/src/components/SearchResultItem/index.js
+++ frontend/src/components/SearchResultItem/index.js
@@ -1,15 +1,31 @@
 import "./index.css"
+import { Modal } from "../../context/Modal";
+import { useState } from "react";
+import BusinessPage from "../BusinessPage";
+
 const SearchResultItem = ({business}) => {
-    console.log(business)
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShowModal = () => {
+        setShowModal(true)
+        console.log("hi")
+        console.log(showModal)
+    }
     return (
-        <div className="search-item-container">
-            <img src={`${business.imageUrl}`} alt="business-photo" className="item-img"/>
-            <div className="search-item-info">
-                <h1 className="search-business-name">{business.name}</h1>
-                <p>{business.location.address1}</p>
+        <>
+            <div onClick={handleShowModal} className="search-item-container">
+                <img src={`${business.imageUrl}`} alt={business.name} className="item-img"/>
+                <div className="search-item-info">
+                    <h1 className="search-business-name">{business.name}</h1>
+                    <p>{business.location.address1}</p>
+                </div>
             </div>
-            
-        </div>
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <BusinessPage business={business} />
+                </Modal>
+            )}
+        </>
     )
 };
 

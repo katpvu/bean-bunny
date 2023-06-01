74e2e75442c0479827cb5f3aecbd1df0e44c416b
katpvu <vu.kathy98@gmail.com>
2023-05-31 23:24:39 -0700

    change styling for login and signup page

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -5,9 +5,7 @@ import { login } from '../../store/session';
 import { Redirect } from 'react-router-dom';
 import { checkErrors } from '../../utils';
 import { Link, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import bean from '../../assets/bean-logo-black.png'
-import loginBeans from '../../assets/login-beans.jpg'
-import SplashPage from "../SplashPage"
+import spilledBeans from "../../assets/spilled-beans.png"
 
 const LoginFormPage = (props) => {
     // hooks
@@ -45,70 +43,39 @@ const LoginFormPage = (props) => {
             .then(history.push("/"))
     };
 
-    const styles = {
-        center: "center",
-        B: "create-acc-line",
-        C: "form-input-container",
-        D: "submit-buttons-container",
-        E: "bunny-logo",
-        F: "center",
-        G: "errors",
-        H: "login-container"
-    };
-
-    const eventHandler = {
-        onGetStarted: () => setSplash(false) 
-    }
-
     return (
-
         <>
-
-        {/* {splash && (
-            <SplashPage eHandler={eventHandler}/>
-            
-        )} */}
-        
-        {/* {!splash && ( */}
-        <div className={`${styles.H}`}>
-            <div className="left-half">
-                <img src={loginBeans} />
-            </div>
-
-            <div className="right-half">
-                <div className={`${styles.F}`}>
-                    <img src={bean} className={`${styles.E}`} alt="logo"/>
+        <div className="login-container">
+            <h2 className="login-form-title">Welcome back</h2>
+            <form onSubmit={handleSubmit} className="form-container">
+                <ul>
+                    {errors.map(error => <li key={error}>{error}</li>)}
+                </ul>
+                <input 
+                    required 
+                    className="sign-in-text-box" 
+                    type="text" 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="username" 
+                />
+                <input 
+                    required 
+                    className="sign-in-text-box" 
+                    type="password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    placeholder="password"
+                />
+                <div className="create-acc-line" >
+                    <p >Not a member yet? <Link to="/signup">Create an account</Link></p>
                 </div>
-                <h2 className="login-form-title">Welcome back!</h2>
-                <form onSubmit={handleSubmit} className="form-container">
-                    <ul>
-                        {errors.map(error => <li className={`${styles.G} ${styles.center}`}key={error}>{error}</li>)}
-                    </ul>
+                    <button className="login-button">Sign In</button>
 
-                    <div className={`${styles.center} ${styles.C}`}>
-                        <div className="input-container">
-                            <label >Username
-                                <input required className="sign-in-text-box" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                            </label>
-                        </div>
-                        <div className="input-container">
-                            <label>Password
-                                <input required className="sign-in-text-box" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                            </label>
-                        </div>
-                    </div>
-                    <div className={`${styles.center} ${styles.B}`} >
-                        <p >Not a member yet? <Link to="/signup">Create an account</Link></p>
-                    </div>
-                    <div className={`${styles.center} ${styles.D}`}>
-                        <button className="login-button">Sign In</button>
-
-                        <button className="login-button" onClick={handleDemoLogIn}>Log In as Demo User!</button>
-                    </div>
-                </form>
-            </div>
+                    <button className="login-button" onClick={handleDemoLogIn}>Login as Demo User</button>
+            </form>
+            <img className="login-beans" src={spilledBeans} alt="background" />
         </div>
-        {/* )} */}
         </>
     )
 
--- frontend/src/components/SignUpForm/index.js
+++ frontend/src/components/SignUpForm/index.js
@@ -1,12 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
-import './index.css'
+// import './index.css'
 import { signup } from '../../store/session';
 import { Redirect } from 'react-router-dom';
 import { checkErrors } from '../../utils';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import bean from '../../assets/bean-logo-black.png'
-import loginBeans from '../../assets/login-beans.jpg'
+import spilledBeans from "../../assets/spilled-beans.png"
+
 
 const SignUpForm = (props) => {
 
@@ -33,59 +33,42 @@ const SignUpForm = (props) => {
             });
     };
 
-    const styles = {
-        A: "center",
-        B: "create-acc-line",
-        C: "form-input-container",
-        D: "submit-buttons-container",
-        E: "errors",
-        F: "bunny-logo",
-        G: "input-container",
-        H: "login-container"
-    }
-
-
     return (
-        <div className={`${styles.A} ${styles.H}`}>
-            <div className="left-half">
-                <img src={loginBeans} />
-            </div>
-
-            <div className="right-half">
-                <div className={`${styles.A}`}><img src={bean} className={`${styles.F}`} alt="logo"/></div>
+        <div className="login-container">
                 <h2 className="login-form-title">Create an Account</h2>
                 <form onSubmit={handleSubmit} className="form-container">
-                    <ul className={`${styles.A} ${styles.E}`}>
+                    <ul className="errors">
                         {errors.map(error => <li key={error}>{error}</li>)}
                     </ul>
-
-                    <div className={`${styles.A} ${styles.C}`}>
-                        <div className={`${styles.G} ${styles.A}`}>
-                            <label >Email
-                                <input required className="sign-in-text-box" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            </label>
-                        </div>
-                        <div className={`${styles.G} ${styles.A}`}>
-                            <label >Username
-                                <input required className="sign-in-text-box" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                            </label>
-                        </div>
-                        <div className={`${styles.G} ${styles.A}`}>
-                            <label>Password
-                                <input required className="sign-in-text-box" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                            </label>
-                        </div>
+                    <input 
+                        required 
+                        className="sign-in-text-box" 
+                        type="text" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        placeholder="email"/>
+                    <input 
+                        required 
+                        className="sign-in-text-box" 
+                        type="text" 
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)} 
+                        placeholder="username"/>
+                    <input 
+                        required 
+                        className="sign-in-text-box" 
+                        type="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        placeholder="password"/>
+                    <div className="create-acc-line">
+                        <p>Already have an account? <Link to="/login">Sign in here!</Link></p>
                     </div>
-                    <div className={`${styles.A} ${styles.B}`}>
-                        <p >Already have an account? <Link to="/login">Sign in here!</Link></p>
-                    </div>
-                    <div className={`${styles.A} ${styles.D}`}>
-                        <button className="login-button">Sign Up</button>
-                    </div>
+                    <button className="login-button">Sign Up</button>
                 </form>
+                <img className="login-beans" src={spilledBeans} alt="background" />
 
 
-            </div>
         </div>
     )
 };

961496dd7feb0244404a6d8276f9ffd865bd70af
katpvu <vu.kathy98@gmail.com>
2023-06-06 19:42:08 -0700

    add loaders for when a user logs in

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -2,13 +2,14 @@ import coffeeBeans from "../../assets/coffee-beans-splash.png"
 import "./index.css";
 
 const SplashPage = () => {
+
     return (
-    <div id="splash-root">
-        <h1 id="splash-logo">FIND YOUR CUP OF COFFEE</h1>
-        <div id="splash-img-container">
-            <img src={coffeeBeans} alt="splash-img" id="splash-img"/>
+        <div id="splash-root">
+            <h1 id="splash-logo">FIND YOUR CUP OF COFFEE</h1>
+            <div id="splash-img-container">
+                <img src={coffeeBeans} alt="splash-img" id="splash-img"/>
+            </div>
         </div>
-    </div>
     );
 };
 

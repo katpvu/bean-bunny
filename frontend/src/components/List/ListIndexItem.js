9f4c44f211cfaff1291f3c5f76edb71708f1b987
katpvu <vu.kathy98@gmail.com>
2023-06-12 11:18:04 -0700

    add collection images

--- frontend/src/components/List/ListIndexItem.js
+++ frontend/src/components/List/ListIndexItem.js
@@ -6,6 +6,7 @@ const ListIndexItem = ({list}) => {
     return (
         <>
             <Link className="list-item" to={`/lists/${list.id}`}>
+                <img src={list.collectionImage} alt="collection" />
                 <h1>{list?.title}</h1>
                 <p>{list?.numListItems} coffee shops saved</p>
             </Link>
